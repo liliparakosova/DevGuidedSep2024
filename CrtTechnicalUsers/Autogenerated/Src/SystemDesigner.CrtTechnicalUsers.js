14089ebba644d944a6d5f6a456e2ba42b20ad14a
Lilyana Parakosova <l_parakosova@tbiinfo.eu>
2024-09-20 10:34:50 +0300

    Initial commit

---
+++ CrtTechnicalUsers/Autogenerated/Src/SystemDesigner.CrtTechnicalUsers.js
@@ -0,0 +1,63 @@
+﻿  define("SystemDesigner", ["SystemDesignerResources"], function (resources) {
+	return {
+		attributes: {
+			/**
+			 * Flag that indicates feature "EnableTechnicalUser" enabled.
+			 */
+			"IsTechnicalUsersEnabled": {
+				dataValueType: this.Terrasoft.DataValueType.BOOLEAN,
+				type: this.Terrasoft.ViewModelColumnType.VIRTUAL_COLUMN,
+				value: false
+			}
+		},
+		methods: {
+			/**
+			 * @inheritodoc SystemDesigner#getOperationRightsDecoupling
+			 * @overridden
+			 */
+			getOperationRightsDecoupling: function() {
+				var operationRights = this.callParent(arguments);
+				operationRights.navigateToTechnicalUserSection = "CanManageTechnicalUsers";
+				return operationRights;
+			},
+			/**
+			 * @inheritodoc SystemDesigner#setDefaultParameters
+			 * @overridden
+			 */
+			setDefaultParameters: function() {
+				this.callParent(arguments);
+				var isEnabled = this.getIsFeatureEnabled("EnableTechnicalUser");
+				this.$IsTechnicalUsersEnabled = isEnabled;
+			},
+			/**
+			 * Navigate to the Technical Users section.
+			 * @private
+			 */
+			_navigateToTechnicalUsersSection: function() {
+				if (this.get("CanManageTechnicalUsers") === true) {
+					var url = Terrasoft.workspaceBaseUrl + "/Shell/#Page/TechnicalUsers_ListPage";
+					window.open(url, "_self");
+				} else {
+					this.showPermissionsErrorMessage("CanManageTechnicalUsers");
+				}
+			}
+
+		},
+		diff: [
+			{
+				"operation": "insert",
+				"propertyName": "items",
+				"parentName": "IntegrationTile",
+				"name": "TechnicalUserSection",
+				"values": {
+					"itemType": Terrasoft.ViewItemType.LINK,
+					"caption": { "bindTo": "Resources.Strings.TechnicalUsersSectionLinkCaption" },
+					"tag": "_navigateToTechnicalUsersSection",
+					"click": "$invokeOperation",
+					"visible": "$IsTechnicalUsersEnabled",
+                    "isNew": true
+				}
+			}
+		]
+	};
+});
